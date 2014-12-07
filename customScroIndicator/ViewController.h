808e43be7e8231c669143fa7f6cdc0782cabbea7
bo song <songbo.dian@gmail.com>
2014-12-07 19:26:24 +0800

    Initial Commit

---
+++ customScroIndicator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  customScroIndicator
+//
+//  Created by songbo on 14-12-7.
+//  Copyright (c) 2014年 songbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
